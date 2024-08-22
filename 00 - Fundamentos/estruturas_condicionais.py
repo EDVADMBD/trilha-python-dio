5f4d128a654d24817de54e96541e3ef999cd6629
EDVALDO DIAS DA SILVA <edvdias01@gmail.com>
2024-08-22 11:43:34 -0300

    Update estruturas_condicionais.py

--- 00 - Fundamentos/estruturas_condicionais.py
+++ 00 - Fundamentos/estruturas_condicionais.py
@@ -22,3 +22,22 @@ elif idade == IDADE_ESPECIAL:
     print("Pode fazer aulas teóricas, mas não pode fazer aulas práticas.")
 else:
     print("Ainda não pode tirar a CNH.")
+
+
+====meu código=====
+
+
+MAIOR_IDADE = 18
+IDADE_ESPECIAL = 17
+IDADE_ESPECIAL_M = 75
+
+idade = int(input("Informe sua idade: "))
+
+if idade >= IDADE_ESPECIAL_M:
+    print("Realizar exames, Não pode renovar a CNH.")
+elif idade >= MAIOR_IDADE:
+    print("Maior de idade, pode tirar a CNH.")
+elif idade == IDADE_ESPECIAL:
+    print("Pode fazer aulas teóricas, mas não pode fazer aulas práticas.")
+else:
+    print("Ainda não pode tirar a CNH.")
